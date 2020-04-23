bb52f6aea7f749d4c13c560014c1136b126e9c9a
pardusurbanus <pardusurbanus@gmail.com>
2020-04-24 05:32:47 +0700

    upd: model, migrations, table

---
+++ app/DataTables/HomeDeliveryOrderDataTable.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * HomeDeliveryOrders DataTable
+ *
+ * @package     Rideon Driver
+ * @subpackage  DataTable
+ * @category    HomeDelivery
+ * @author      pardusurbanus@gmail.com
+ * @version     2.2
+ * @link        http://trioangle.com
+ */
+
+namespace App\DataTables;
+
+use App\Models\User;
+use App\Models\HomeDeliveryOrder;
+use Yajra\DataTables\Services\DataTable;
+use DB;
+
+class HomeDeliveryOrderDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->of($query)
+            ->addColumn('email', function ($users) {
+                return protectedString($users->email);
+            })
+            ->addColumn('action', function ($users) {
+                $edit = (LOGIN_USER_TYPE=='company' || auth('admin')->user()->can('update_driver')) ? '<a href="'.url(LOGIN_USER_TYPE.'/edit_driver/'.$users->id).'" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;' : '';
+                $delete = (auth()->guard('company')->user()!=null || auth('admin')->user()->can('delete_driver')) ? '<a data-href="'.url(LOGIN_USER_TYPE.'/delete_driver/'.$users->id).'" class="btn btn-xs btn-primary" data-toggle="modal" data-target="#confirm-delete"><i class="glyphicon glyphicon-trash"></i></a>&nbsp;':'';
+                return $edit.$delete;
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        $orders = DB::Table('delivery_orders')->select('delivery_orders.id', 'delivery_orders.status', 'delivery_orders.estimate_time', DB::raw('CONCAT(delivery_orders.fee, delivery_orders.currency_code) AS fee'));
+        $users = DB::Table('users')->select('users.id as id', 'users.first_name', 'users.last_name','users.email','users.country_code','users.mobile_number', 'users.status','companies.name as company_name','users.created_at',DB::raw('CONCAT("XXXXXX",Right(users.mobile_number,4)) AS hidden_mobile'))
+            ->leftJoin('companies', function($join) {
+                $join->on('users.company_id', '=', 'companies.id');
+            })->where('user_type','Driver')->groupBy('id');
+
+        //If login user is company then get that company drivers only
+        if (LOGIN_USER_TYPE=='company') {
+            $users = $users->where('company_id',auth()->guard('company')->user()->id);
+        }
+        return $users;
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('lBfr<"table-responsive"t>ip')
+                    ->orderBy(0)
+                    ->buttons(
+                        ['csv', 'excel', 'print', 'reset']
+                    );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        $mobile_number_column = (isLiveEnv())?'hidden_mobile':'mobile_number';
+        $columns = [
+            ['data' => 'id', 'name' => 'users.id', 'title' => 'Id'],
+            ['data' => 'first_name', 'name' => 'users.first_name', 'title' => 'First Name'],
+            ['data' => 'last_name', 'name' => 'users.last_name', 'title' => 'Last Name'],
+        ];
+        if (LOGIN_USER_TYPE!='company') {
+            $columns[] = ['data' => 'company_name', 'name' => 'companies.name', 'title' => 'Company Name'];
+        }
+        $more_columns = [
+            ['data' => 'email', 'name' => 'users.email', 'title' => 'Email'],
+            ['data' => 'status', 'name' => 'users.status', 'title' => 'Status'],
+            ['data' => $mobile_number_column, 'name' => 'users.mobile_number', 'title' => 'Mobile Number'],
+            ['data' => 'created_at', 'name' => 'users.created_at', 'title' => 'Created At'],
+            ['data' => 'action', 'name' => 'action', 'title' => 'Action', 'orderable' => false, 'searchable' => false, 'exportable' => false],
+        ];
+
+        return array_merge($columns,$more_columns);
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'home_delivery_' . date('YmdHis');
+    }
+}
