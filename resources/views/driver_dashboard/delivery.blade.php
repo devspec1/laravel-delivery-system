910632efe45b1c504e999b7ac8c6c169f7169d65
Julien Moutier <julienmoutier@hotmail.com>
2020-05-11 07:00:19 +0200

    Delivery page test

--- resources/views/driver_dashboard/delivery.blade.php
+++ resources/views/driver_dashboard/delivery.blade.php
@@ -4,55 +4,74 @@
 <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 flexbox__item four-fifths page-content" style="padding:0px !important;" ng-controller="facebook_account_kit">
   @include('common.driver_dashboard_header_new')
   <div id="deliveryMainWrp">
-           <div style="display: flex; justify-content: space-between;width: 100%">
-              <span style="font-size: 200%; color: #1B187F; font-weight: normal; font-family:'MontserratBold'">Delivery Orders</span>
-              <div style="display: flex" id="deliveryKmWrp">
-                <span style="border-top-left-radius: 7px; border-bottom-left-radius: 7px; color: #1B187F; font-weight: bold; font-family: 'MontserratBold'">5 km</span>
-                <span>15 km</span>
-                <span style="border-top-right-radius: 7px; border-bottom-right-radius: 7px; ">25 km</span>
-              </div>
+           <div style="display: flex;width: 100%">
+              <span style="font-size: 200%; color: #1B187F; font-weight: normal; font-family:'MontserratBold'">Delivery</span>
+              
             </div>
             <div style="display: flex; flex-direction: column; width: 100%" id="deliverySubWrp">
-              <div>
-                <span>Date & Time</span>
-                <span>Pick up</span>
-                <span>Est. distance</span>
-                <span>Time</span>
-                <span>Fee</span>
-                <span style="width: 8%;text-align: center;">Action</span>
+              <div class="deliverySubHeader">
+                <span>Order Completed</span>
+                <span>Venue</span>
+                <span>Address</span>
+                <span>Driver Name</span>
+                <span>Total</span>
+               
               </div>
                <div>
-                <span>5 Apr. 2020 | 20:00</span>
-                <span>Funky, Thai, Keilor</span>
-                <span>3km</span>
-                <span>Now</span>
-                <span>$ 30.00 USD</span>
-                <span class="acceptBtn1">Accept</span>
+                <div> <span>04/02/2020 | 11:25</span> <span> Order No. 1246098 </span> </div>
+               
+                <span>The Ants Cafe, 2259 14A</span>
+                <span>The Ants Cafe, 2259 14A</span>
+               <span>Joe Corrigan</span>
+                <span>$76.00</span>
+                
               </div>
               <div>
-                <span>5 Apr. 2020 | 20:00</span>
-                <span>Funky, Thai, Keilor</span>
-                <span>3km</span>
-                <span>Now</span>
-                <span>$ 30.00 USD</span>
-                <span class="acceptBtn1">Accept</span>
+                <div> <span>04/02/2020 | 11:25</span> <span> Order No. 1246098 </span> </div>
+               
+                <span>The Ants Cafe, 2259 14A</span>
+                <span>The Ants Cafe, 2259 14A</span>
+               <span>Joe Corrigan</span>
+                <span>$76.00</span>
+                
               </div>
               <div>
-                <span>5 Apr. 2020 | 20:00</span>
-                <span>Funky, Thai, Keilor</span>
-                <span>3km</span>
-                <span>Now</span>
-                <span>$ 30.00 USD</span>
-                <span class="acceptBtn1">Accept</span>
+                <div> <span>04/02/2020 | 11:25</span> <span> Order No. 1246098 </span> </div>
+               
+                <span>The Ants Cafe, 2259 14A</span>
+                <span>The Ants Cafe, 2259 14A</span>
+               <span>Joe Corrigan</span>
+                <span>$76.00</span>
+                
               </div>
               <div>
-                <span>5 Apr. 2020 | 20:00</span>
-                <span>Funky, Thai, Keilor</span>
-                <span>3km</span>
-                <span>Now</span>
-                <span>$ 30.00 USD</span>
-                <span class="acceptBtn1">Accept</span>
+                <div> <span>04/02/2020 | 11:25</span> <span> Order No. 1246098 </span> </div>
+               
+                <span>The Ants Cafe, 2259 14A</span>
+                <span>The Ants Cafe, 2259 14A</span>
+               <span>Joe Corrigan</span>
+                <span>$76.00</span>
+                
               </div>
+              <div>
+                <div> <span>04/02/2020 | 11:25</span> <span> Order No. 1246098 </span> </div>
+               
+                <span>The Ants Cafe, 2259 14A</span>
+                <span>The Ants Cafe, 2259 14A</span>
+               <span>Joe Corrigan</span>
+                <span>$76.00</span>
+                
+              </div>
+              <div>
+                <div> <span>04/02/2020 | 11:25</span> <span> Order No. 1246098 </span> </div>
+               
+                <span>The Ants Cafe, 2259 14A</span>
+                <span>The Ants Cafe, 2259 14A</span>
+               <span>Joe Corrigan</span>
+                <span>$76.00</span>
+                
+              </div>
+              
             </div>
 
 
