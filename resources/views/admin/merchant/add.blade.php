8fab88932f009ea261394a0a5c0cbcc5e6814377
dev <blightdev@yandex.com>
2020-05-13 11:09:55 +0800

    Update Merchant Management

--- resources/views/admin/merchant/add.blade.php
+++ resources/views/admin/merchant/add.blade.php
@@ -44,8 +44,84 @@
                                 <span class="text-danger">{{ $errors->first('integration_type') }}</span>
                            </div>	
                         </div>
-
-                        
+						<div class="form-group">
+							<label for="input_first_name" class="col-sm-3 control-label">First Name<em class="text-danger">*</em></label>
+							<div class="col-sm-6">
+								{!! Form::text('first_name', '', ['class' => 'form-control', 'id' => 'input_first_name', 'placeholder' => 'First Name']) !!}
+								<span class="text-danger">{{ $errors->first('first_name') }}</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="input_last_name" class="col-sm-3 control-label">Last Name<em class="text-danger">*</em></label>
+							<div class="col-sm-6">
+								{!! Form::text('last_name', '', ['class' => 'form-control', 'id' => 'input_last_name', 'placeholder' => 'Last Name']) !!}
+								<span class="text-danger">{{ $errors->first('last_name') }}</span>
+							</div>
+                        </div>
+						<div class="form-group">
+							<label for="input_email" class="col-sm-3 control-label">Email<em class="text-danger">*</em></label>
+							<div class="col-sm-6">
+								{!! Form::text('email', '', ['class' => 'form-control', 'id' => 'input_email', 'placeholder' => 'Email']) !!}
+								<span class="text-danger">{{ $errors->first('email') }}</span>
+							</div>
+						</div>
+						{!! Form::hidden('user_type','Merchant', ['class' => 'form-control', 'id' => 'user_type', 'placeholder' => 'Select']) !!}						
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">Mobile Number </label>
+							<div class="col-sm-6">
+								{!! Form::text('mobile_number','', ['class' => 'form-control', 'id' => 'mobile_number', 'placeholder' => 'Mobile Number']) !!}
+								<span class="text-danger">{{ $errors->first('mobile_number') }}</span>
+							</div>
+						</div>						
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">Address Line 1 </label>
+							<div class="col-sm-6">
+								{!! Form::text('address_line1','', ['class' => 'form-control', 'id' => 'address_line1', 'placeholder' => 'Address Line 1']) !!}
+								<span class="text-danger">{{ $errors->first('address_line1') }}</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">Address Line 2 </label>
+							<div class="col-sm-6">
+								{!! Form::text('address_line2','', ['class' => 'form-control', 'id' => 'address_line2', 'placeholder' => 'Address Line 2']) !!}
+								<span class="text-danger">{{ $errors->first('address_line2') }}</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">City </label>
+							<div class="col-sm-6">
+								
+								{!! Form::text('city','', ['class' => 'form-control', 'id' => 'city', 'placeholder' => 'City']) !!}
+								<span class="text-danger">{{ $errors->first('city') }}</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">State</label>
+							<div class="col-sm-6">
+								
+								{!! Form::text('state','', ['class' => 'form-control', 'id' => 'state', 'placeholder' => 'State']) !!}
+								<span class="text-danger">{{ $errors->first('state') }}</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">Postal Code </label>
+							<div class="col-sm-6">
+								{!! Form::text('postal_code','', ['class' => 'form-control', 'id' => 'postal_code', 'placeholder' => 'Postal Code']) !!}
+								<span class="text-danger">{{ $errors->first('postal_code') }}</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="input_status" class="col-sm-3 control-label">Country Code<em class="text-danger">*</em></label>
+							<div class="col-sm-6">
+								<select class ='form-control' id = 'input_status' name='country_code'>
+									<option value="" disabled=""> Select </option>
+									@foreach($country_code_option as $country_code)
+									<option value="{{@$country_code->phone_code}}">{{$country_code->long_name}}</option>
+									@endforeach
+								</select>
+								<span class="text-danger">{{ $errors->first('country_code') }}</span>
+							</div>
+						</div>
 					</div>
 					<div class="box-footer">
 						<button type="submit" class="btn btn-info pull-right" name="submit" value="submit">Submit</button>
