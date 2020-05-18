c7d5f0e7dda9b669d0362e75473c3bd902756cf7
Julien Moutier <julienmoutier@hotmail.com>
2020-05-18 12:28:55 +0200

    Password reset 1

--- resources/views/driver_dashboard/forget_password.blade.php
+++ resources/views/driver_dashboard/forget_password.blade.php
@@ -2,27 +2,23 @@
 @section('main')
 <style> body, html { height: 100%; }</style>
 <div id="newLoginWrp" style="height: 100%; width: 100%; display: flex; justify-content: center; align-items: center">
-    <form id="newLoginForm" action="/login_driver" method="post" style="height: 80%; width: 40%; padding: 1em; padding-top: 4em; padding-bottom: 4em; display: flex; flex-direction: column; align-items: center; justify-content: space-between; box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.25); font-family: 'MontserratReg">
+    <form id="newLoginForm" action="{{ url('driver/reset_password') }}" method="post" style="height: 80%; width: 40%; padding: 1em; padding-top: 4em; padding-bottom: 4em; display: flex; flex-direction: column; align-items: center; justify-content: space-between; box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.25); font-family: 'MontserratReg">
 
         @csrf
 
         <img src="{{ asset('images/logos/logo.png') }}" style="height: 13em">
-
-        <h1 style="font-family: 'MontserratReg'; font-weight: bold; font-size: 170%">Login</h1>
+         <div style="display: flex; flex-direction: column; width: 100%; align-items: center">
+        <h1 style="font-family: 'MontserratReg'; font-weight: bold; font-size: 170%">Reset password</h1>
         @if($errors->any())
         <h4 style="font-size: 110%; color: #a84632">{{$errors->first()}}</h4>
         @endif
-        @if(Session::has('success'))
-        <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('success') }}</p>
-        @endif
        
-         <div style="display: flex; align-items: center; margin-top: 0.4em;margin-bottom: 0.9em;padding: 1.5em; width: 65%; background: rgba(0, 0, 0, 0.02); border:1px solid rgba(0, 0, 0, 0.12)"> <img src="{{ asset('images/icon/mail.png')}}" style="heigth: 1.3em; width: 1.4em; opacity: 0.5; margin-right: 2em"> <input style="border: none; background: transparent; font-size: 90%; font-weight: bold" type="text" name="email" placeholder="Email"> </div>
-        <div style="display: flex; align-items: center; margin-top: 0.4em;margin-bottom: 0.9em;padding: 1.5em; width: 65%; background: rgba(0, 0, 0, 0.02); border:1px solid rgba(0, 0, 0, 0.12)"> <img src="{{ asset('images/icon/password.png')}}" style="heigth: 1.3em; width: 1.4em; opacity: 0.7; margin-right: 2em"> <input style="border: none; background: transparent; font-size: 90%; font-weight: bold" type="password" name="password" placeholder="Password"> </div>
-         <span class="span1"> <a style="color: black;" href="{{ url('/driver/forget_password') }}">Forgot password ?</a></span>
-        <span class="spanB1" id="loginBtn" style="background-color: #3B5998;">Login <img src="{{ asset('images/icon/next.png') }}" style="position: relative; left: 6em;  height: 1.2em; width: 1.2em"></span>
+         <div style="display: flex; align-items: center; margin-top: 1.4em;margin-bottom: 2.9em;padding: 1.5em; width: 65%; background: rgba(0, 0, 0, 0.02); border:1px solid rgba(0, 0, 0, 0.12)"> <img src="{{ asset('images/icon/mail.png')}}" style="heigth: 1.3em; width: 1.4em; opacity: 0.5; margin-right: 2em"> <input style="border: none; background: transparent; font-size: 90%; font-weight: bold; width: 100%" type="text" name="email" placeholder="Email"> </div>
+    
+        <span class="spanB1" id="loginBtn" style="background-color: #3B5998;">Send link <img src="{{ asset('images/icon/next.png') }}" style="position: relative; left: 6em;  height: 1.2em; width: 1.2em"></span>
+    </div>
+    <div></div>
 
-        <span class="span1">Don't have account ? </span>
-        <a href="/driver/new_signup" class="spanB1" style="text-decoration: none; background-color: #4B4B4B;"> <span>Apply</span></a>
 
     </form>
 </div>
