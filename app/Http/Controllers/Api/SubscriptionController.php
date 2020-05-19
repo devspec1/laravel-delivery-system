c2925a517b1a79c8bac6ef502ef28519b9eccf8c
pardusurbanus <pardusurbanus@gmail.com>
2020-05-19 16:11:35 +0700

    add_card_details

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -498,21 +498,31 @@ class SubscriptionController extends Controller
             else{
                 if(!$subscription_row->stripe_id){
                     $plan = StripeSubscriptionsPlans::where('plan_name','Member Driver')->first();
-                    $payment_method = \Stripe\PaymentMethod::create([
-                        'type' => 'card',
-                        'card' => [
-                          'number' => '4242424242424242',
-                          'exp_month' => 5,
-                          'exp_year' => 2021,
-                          'cvc' => '314',
-                        ],
-                      ]);
-                    
-                    $request->intent_id = $payment_method->id;
-                    return response()->json($request);
+                    // $payment_method = \Stripe\PaymentMethod::create([
+                    //     'type' => 'card',
+                    //     'card' => [
+                    //       'number' => '4242424242424242',
+                    //       'exp_month' => 5,
+                    //       'exp_year' => 2021,
+                    //       'cvc' => '314',
+                    //     ],
+                    //   ]);
+                    // $setup_intent = \Stripe\SetupIntent::create([
+                    //     'payment_method_types' 	=> ['card'],
+                    //       'customer'				=> $payment_details->customer_id,
+                    //       'usage'					=> 'off_session',
+                    // ]);
+                    // $setup_intent = \Stripe\SetupIntent::update(
+                    //     $setup_intent->id,
+                    //     ['payment_method' => $payment_method],
+                    // );
+                    // $setup_intent->confirm([
+                    //     'payment_method' => $payment_method->id,
+                    //   ]);
                     $payment = resolve('App\Http\Controllers\Api\ProfileController');
-                    $res = $payment->add_card_details($request);
-                    
+                    $request->request->add(['intent_id' => $request->payment_method]);
+                    $payment->add_card_details($request);
+
                     try{
                         $subscription = \Stripe\Subscription::create([
                             'customer' => $payment_details->customer_id,
